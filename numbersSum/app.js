5d67d56241544e23cb5778a0c79f498a2e7b620d
StasTikh <tmihatelefan@gmail.com>
2021-07-05 18:32:34 +0300

    some fixes

--- numbersSum/app.js
+++ numbersSum/app.js
@@ -1,26 +1,18 @@
 function isNumber() {
     let answer1 = +startNum.value;
     let answer2 = +endNum.value;
-    let alert = document.getElementById('noNum');
+    let alert1 = document.getElementById('noNum');
     let alert2 = document.getElementById('noNum2');
     
     if(Number.isNaN(answer1)){
-        alert.style.display = "inline";
-        alert.innerHTML = "Ввод только чисел"
-        
+        alert1.style.display = "inline";
+        alert1.innerHTML = "Ввод только чисел"    
     } else if (Number.isNaN(answer2)) {
         alert2.style.display = "inline";
         alert2.innerHTML = "Ввод только чисел"
-    } else if (answer1 === "0") {
-        alert.style.display = "inline";
-        alert.innerHTML = "Не меньше единицы";
-    } else if (answer2 === "0") {
-        alert2.style.display = "inline";
-        alert2.innerHTML = "Не меньше единицы";
     } else {
-        alert.style.display = "none";
+        alert1.style.display = "none";
         alert2.style.display = "none";
-        
     }
 
 }
