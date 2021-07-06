3e860a9b99022ee8f0f2a562b6da9c7a199012cf
StasTikh <tmihatelefan@gmail.com>
2021-07-06 17:03:04 +0300

    исправления недочета попыток и нажатия кнопок при больше 100 и меньше 1

--- guessGame/app.js
+++ guessGame/app.js
@@ -18,20 +18,53 @@
       win.style.display = "block";
     }); 
         
+    
+    
+    
     function search(){return Math.floor( (low + high)/2 );}       
 
     
     small.addEventListener('click', function(){
         high = Number(answerF.innerHTML);
         answerF.innerHTML = search();
-        numGuess++;  
+        numGuess++;
+        if (numGuess == 10) {
+          small.style.display = "none";
+          big.style.display = "none";
+          win.style.display = "none";
+          restart.style.display = "block";
+          document.getElementById("winField").innerHTML = `Попытки закончились, я проиграл!`;    
+        } else if( search() == 0) {
+          small.style.display = "none";
+          big.style.display = "none";
+          win.style.display = "none";
+          restart.style.display = "block";
+          answerF.innerHTML = `Нельзя меньше 1!`;
+          document.getElementById("winField").innerHTML = `Правила для кого написаны?`;
+        }  
       });
       
       big.addEventListener('click', function(){
         low = Number(answerF.innerHTML);    
         answerF.innerHTML = search();
         numGuess++;
+        if (numGuess == 10) {
+          small.style.display = "none";
+          big.style.display = "none";
+          win.style.display = "none";
+          restart.style.display = "block";
+          document.getElementById("winField").innerHTML = `Попытки закончились, я проиграл!`;    
+        } else if( search() == 100) {
+          small.style.display = "none";
+          big.style.display = "none";
+          win.style.display = "none";
+          restart.style.display = "block";
+          answerF.innerHTML = `А куда уж больше 100?`;
+          document.getElementById("winField").innerHTML = `Правила для кого написаны?`;
+        }  
       });
+
+      
     
     win.addEventListener("click", function(){
         small.style.display = "none";
