3e63fc58a2aa2a0421c6daeb4c77f8eb1df1d79e
StasTikh <82860582+StasTikh@users.noreply.github.com>
2021-10-13 11:23:14 +0300

    Delete js13-homework2-3 directory

--- js13-homework2-3/assets/js/IsNumber.js
+++
@@ -1,9 +0,0 @@
-function isNumber() {
-    let answer1 = +inputCode.value.replace(/\s+/g, '')
-      
-    if(Number.isNaN(answer1)){
-      outCorr.innerHTML = "Only numbers";
-      answer1 = ""; 
-    } 
-  }
-  
